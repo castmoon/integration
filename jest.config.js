387618e2d4d84830efc2ab0e3f7c1931a01a382b
Guilherme Alves <academicgmag@gmail.com>
2021-12-10 11:41:55 -0300

    chore: add jest configs

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    transform: {
+        '.+\\.ts$': 'ts-jest',
+    },
+};
---
+++ src/presentation/controllers/Oportunity-controller.spec.ts
@@ -0,0 +1,5 @@
+describe('Name of the group', () => {
+  test('should ', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
